bd1d63ed0033f30d530c14aa83efbec78a803ce9
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-08-26 18:05:25 +0800

    resolve conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,6 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PositionsComponent } from './positions/positions.component';
 import { FormsModule } from '@angular/forms';
 
-import {FormsModule} from "@angular/forms";
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,11 +26,7 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     HttpClientModule,
-<<<<<<< HEAD
-    FormsModule,
-=======
     FormsModule
->>>>>>> 705cf32426f482498ef25c12b88118d24cdad74f
   ],
   providers: [],
   bootstrap: [AppComponent]
